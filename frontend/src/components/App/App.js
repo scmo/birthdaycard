c5e83a9332c572f638c0cb2fecbacc6177cce814
Moritz <schneidermoritz90@gmail.com>
2021-04-05 18:47:19 +0200

    front end with google login

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Contact from '../Contact/Contact'
+import History from '../History/History'
+import Navbar from '../NavBar/NavBar';
+
+
+function App() {
+    const [token, setToken] = useState();
+
+    if (!token) {
+        return (
+            <div className="wrapper">
+                <BrowserRouter>
+                    <Navbar token={token} setToken={setToken} />
+                    <p>Please Login</p>
+                </BrowserRouter>
+            </div>
+        );
+    }
+
+    return (
+        <div className="wrapper">
+            <BrowserRouter>
+                <Navbar token={token} setToken={setToken} />
+                <h1>Application</h1>
+                <Switch>
+                    <Route path="/contact">
+                        <Contact />
+                    </Route>
+                    <Route path="/history">
+                        <History />
+                    </Route>
+
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/components/Login/Login.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Login.css';
+import { useGoogleLogin, useGoogleLogout } from 'react-google-login';
+import Button from '@material-ui/core/Button';
+
+// refresh token
+import { refreshTokenSetup } from '../../utils/refreshToken';
+
+const clientId = process.env.REACT_APP_CLIENT_ID
+
+function Login({ token, setToken }) {
+
+    const onLoginSuccess = (res) => {
+        console.log('Login Success: currentUser:', res.profileObj);
+        alert('Logged in successfully welcome {res.profileObj.name} 😍. \n See console for full profile object.');
+        setToken(res);
+        refreshTokenSetup(res);
+    };
+
+    const onLoginFailure = (res) => {
+        console.log('Login failed: res:', res);
+        alert('Failed to login. 😢 Please ping this to repo owner twitter.com/sivanesh_fiz');
+    };
+    const { signIn } = useGoogleLogin({
+        onSuccess: onLoginSuccess,
+        onFailure: onLoginFailure,
+        clientId: clientId,
+        isSignedIn: true,
+        accessType: 'offline',
+        // responseType: 'code',
+        // prompt: 'consent',
+    });
+
+    const onLogoutSuccess = (res) => {
+        console.log('Logged out Success');
+        alert('Logged out Successfully ✌');
+        setToken(null)
+    };
+
+    const onLogoutFailure = () => {
+        console.log('Handle failure cases');
+        alert('Handle failure cases');
+    };
+
+    const { signOut } = useGoogleLogout({
+        clientId,
+        onLogoutSuccess,
+        onLogoutFailure,
+    });
+
+    if (!token) {
+        return (
+            <Button color="inherit" onClick={signIn} >Login</Button>
+        );
+    } else {
+        return (
+            <Button color="inherit" onClick={signOut} >Logout</Button>
+        );
+    }
+
+}
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+};
+
+export default Login;
